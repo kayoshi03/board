6a13cdf38de686ff3caeef3c6a3284aacc378755
kayoshi03 <r0n1ronov1@gmail.com>
2025-10-05 02:51:17 +0300

    ADD: promo and hero section

---
+++ src/components/blocks/home/heroSection/HeroSection.tsx
@@ -0,0 +1,16 @@
+import "./index.scss"
+
+const HeroSection = () => {
+    return (
+        <div className="hero">
+            <div className="hero__image">
+
+            </div>
+            <div className="hero__text">
+
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection;
---
+++ src/components/blocks/home/promoBlock/PromoBlock.tsx
@@ -0,0 +1,20 @@
+import Button from "@/components/UI/button/Button";
+import "./index.scss";
+import Image from "next/image";
+
+const PromoBlock = () => {
+  return (
+    <div className="promo-block">
+      {/* <div>
+        <h1>CapiBoard</h1>
+        <p>Доска объявлений нового поколения</p>
+        <Button>Создать</Button>
+      </div>
+      <div>
+        <Image src="/capi.png" alt="Promo Background" width={300} height={100} />
+      </div> */}
+    </div>
+  )
+}
+
+export default PromoBlock;
