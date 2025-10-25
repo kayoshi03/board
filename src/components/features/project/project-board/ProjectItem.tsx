ee5a8294029c1547c664a785e9991904a2e3172e
kayoshi03 <r0n1ronov1@gmail.com>
2025-10-25 18:47:17 +0300

    minor

--- src/components/features/project/project-board/ProjectItem.tsx
+++ src/components/features/project/project-board/ProjectItem.tsx
@@ -4,11 +4,16 @@ import { PAGES } from "@/config/routes"
 import { deleteDashboard } from "@/services/api/request/POST/dashboard"
 import { Project } from "@/shared/dashboard.type"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 export const ProjectItem = ({item}:{item: Project}) => {
-
-    const res = async () => await deleteDashboard(item.id)
-
+    const router = useRouter()
+    const res = async () => {
+        const res = await deleteDashboard(item.id)
+        if (res) {
+            router.refresh()
+        }
+    }
     return (
         <div className="board__item">
             <div className="close" onClick={() => res()}>
