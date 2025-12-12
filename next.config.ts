f9ecc6f1b92c6b102e86e65cafbc49f4bcd931d1
kayoshi03 <r0n1ronov1@gmail.com>
2025-12-12 14:20:11 +0500

    refactor: restructure layout and page components; remove unused auth files

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/main",
-        permanent: true
-      }
-    ]
-  },
   async rewrites() {
     return [
       {
